f2b04a66abca92bb530a179ea2c2334bc85ed38d
Jonathan Fok kan <jfokkan@inocybe.com>
2015-01-13 15:38:31 -0500

    ADDED more tests 1 on operator precedence is failing

--- parse/parser_test.go
+++ parse/parser_test.go
@@ -112,6 +112,42 @@ func TestCompositeParen(t *testing.T) {
 	}
 }
 
+func TestSimpleArithmeticPrecedence(t *testing.T) {
+	input := `a+b*2-3/4%a`
+	parser := Parse("TestCompositeParen", input)
+
+	output := parser.File
+	nodeList := []ast.Node{
+		&ast.BinaryExpr{
+			X: &ast.BinaryExpr{
+				X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"a"}},
+				Op: lex.Token{Typ:lex.ADD, Val:"+"},
+				Y: &ast.BinaryExpr{
+					X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"b"}},
+					Op: lex.Token{Typ:lex.MUL, Val:"*"},
+					Y: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"2"}},
+				},
+			},
+			Op: lex.Token{Typ:lex.SUB, Val:"-"},
+			Y: &ast.BinaryExpr{
+				X: &ast.BinaryExpr{
+					X: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"3"}},
+					Op: lex.Token{Typ:lex.QUO, Val:"/"},
+					Y: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+				},
+				Op: lex.Token{Typ:lex.REM, Val:"%"},
+				Y: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"a"}},
+			},
+		},
+	}
+	expected := &ast.File{
+		List: nodeList,
+	}
+	if !ast.Equals(parser.File, expected) {
+		t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", spew.Sdump(expected), spew.Sdump(output))
+	}
+}
+
 func TestMultiExpr(t *testing.T) {
 	input :=
 		`
@@ -148,5 +184,195 @@ func TestMultiExpr(t *testing.T) {
 	}
 	if !ast.Equals(parser.File, expected) {
 		t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", spew.Sdump(expected), spew.Sdump(output))
+	}
+}
+
+func TestMultiExprSemiColonSeperated(t *testing.T) {
+	input :=
+		`
+4+(4);4*4;((100)/90);-aTest;
+`
+	parser := Parse("TestMultiExprSemiColonSeperated", input)
+
+	output := parser.File
+	nodeList := []ast.Node{
+		&ast.BinaryExpr{
+			X: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			Op:lex.Token{Typ:lex.ADD, Val:"+"},
+			Y: &ast.ParenExpr{Lparen: lex.Token{Typ:lex.LEFTPAREN, Val:"("},
+				X: &ast.BasicLit{
+					Tok: lex.Token{
+						Typ:lex.INT,
+						Val:"4",
+					},
+				},
+				Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			},
+		},
+		&ast.BinaryExpr{
+			X: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			Op:lex.Token{Typ:lex.MUL, Val:"*"},
+			Y: &ast.BasicLit{Tok: lex.Token{ Typ:lex.INT, Val:"4"}},
+		},
+		&ast.ParenExpr{
+			Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+			Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			X: &ast.BinaryExpr{
+				X: &ast.ParenExpr{
+					Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+					Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+					X: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"100"}},
+				},
+				Op:lex.Token{Typ:lex.QUO, Val:"/"},
+				Y: &ast.BasicLit{ Tok: lex.Token{Typ:lex.INT, Val:"90"}},
+			},
+		},
+		&ast.UnaryExpr{Op: lex.Token{Typ: lex.SUB, Val: "-"},
+			X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"aTest"}},
+		},
+	}
+	expected := &ast.File{
+		List: nodeList,
+	}
+	if !ast.Equals(parser.File, expected) {
+		// t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", spew.Sdump(expected), spew.Sdump(output))
+		t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", expected.String(), output.String())
+	}
+}
+
+func TestMultiExprMixedSemiColonSeperated(t *testing.T) {
+	input :=
+		`
+4+(4)
+         4*4
+   ((100)/90);-aTest;
+(aoeu)/2222
+`
+	parser := Parse("TestMultiExprMixedSemiColonSeperated", input)
+
+	output := parser.File
+	nodeList := []ast.Node{
+		&ast.BinaryExpr{
+			X: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			Op:lex.Token{Typ:lex.ADD, Val:"+"},
+			Y: &ast.ParenExpr{Lparen: lex.Token{Typ:lex.LEFTPAREN, Val:"("},
+				X: &ast.BasicLit{
+					Tok: lex.Token{
+						Typ:lex.INT,
+						Val:"4",
+					},
+				},
+				Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			},
+		},
+		&ast.BinaryExpr{
+			X: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			Op:lex.Token{Typ:lex.MUL, Val:"*"},
+			Y: &ast.BasicLit{Tok: lex.Token{ Typ:lex.INT, Val:"4"}},
+		},
+		&ast.ParenExpr{
+			Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+			Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			X: &ast.BinaryExpr{
+				X: &ast.ParenExpr{
+					Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+					Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+					X: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"100"}},
+				},
+				Op:lex.Token{Typ:lex.QUO, Val:"/"},
+				Y: &ast.BasicLit{ Tok: lex.Token{Typ:lex.INT, Val:"90"}},
+			},
+		},
+		&ast.UnaryExpr{Op: lex.Token{Typ: lex.SUB, Val: "-"},
+			X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"aTest"}},
+		},
+		&ast.BinaryExpr{
+			X: &ast.ParenExpr{
+				Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+				Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+				X: &ast.Ident{Tok:lex.Token{Typ:lex.IDENTIFIER, Val:"aoeu"}},
+			},
+			Op:lex.Token{Typ:lex.QUO, Val:"/"},
+			Y: &ast.BasicLit{Tok: lex.Token{ Typ:lex.INT, Val:"2222"}},
+		},
+	}
+	expected := &ast.File{
+		List: nodeList,
+	}
+	if !ast.Equals(parser.File, expected) {
+		// t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", spew.Sdump(expected), spew.Sdump(output))
+		t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", expected.String(), output.String())
+	}
+}
+
+func TestMultiLineExprs(t *testing.T) {
+	input :=
+		`
+4+
+(4)
+         4+p*
+4
+   ((100)
+/90);-aTest;
+(aoeu)/2222
+`
+	parser := Parse("TestMultiLineExpressions", input)
+
+	output := parser.File
+	nodeList := []ast.Node{
+		&ast.BinaryExpr{
+			X: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			Op:lex.Token{Typ:lex.ADD, Val:"+"},
+			Y: &ast.ParenExpr{Lparen: lex.Token{Typ:lex.LEFTPAREN, Val:"("},
+				X: &ast.BasicLit{
+					Tok: lex.Token{
+						Typ:lex.INT,
+						Val:"4",
+					},
+				},
+				Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			},
+		},
+		&ast.BinaryExpr{
+			Op:lex.Token{Typ:lex.ADD, Val:"+"},
+			X: &ast.BasicLit{Tok: lex.Token{ Typ:lex.INT, Val:"4"}},
+			Y: &ast.BinaryExpr{
+				Op: lex.Token{Typ:lex.MUL, Val:"*"},
+				X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"p"}},
+				Y: &ast.BasicLit{Tok:lex.Token{Typ:lex.INT, Val:"4"}},
+			},
+		},
+		&ast.ParenExpr{
+			Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+			Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+			X: &ast.BinaryExpr{
+				X: &ast.ParenExpr{
+					Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+					Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+					X: &ast.BasicLit{ Tok:lex.Token{Typ:lex.INT, Val:"100"}},
+				},
+				Op:lex.Token{Typ:lex.QUO, Val:"/"},
+				Y: &ast.BasicLit{ Tok: lex.Token{Typ:lex.INT, Val:"90"}},
+			},
+		},
+		&ast.UnaryExpr{Op: lex.Token{Typ: lex.SUB, Val: "-"},
+			X: &ast.Ident{ Tok: lex.Token{Typ:lex.IDENTIFIER, Val:"aTest"}},
+		},
+		&ast.BinaryExpr{
+			X: &ast.ParenExpr{
+				Lparen: lex.Token{Typ: lex.LEFTPAREN, Val:"("},
+				Rparen: lex.Token{Typ: lex.RIGHTPAREN, Val:")"},
+				X: &ast.Ident{Tok:lex.Token{Typ:lex.IDENTIFIER, Val:"aoeu"}},
+			},
+			Op:lex.Token{Typ:lex.QUO, Val:"/"},
+			Y: &ast.BasicLit{Tok: lex.Token{ Typ:lex.INT, Val:"2222"}},
+		},
+	}
+	expected := &ast.File{
+		List: nodeList,
+	}
+	if !ast.Equals(parser.File, expected) {
+		// t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", spew.Sdump(expected), spew.Sdump(output))
+		t.Errorf("\nExpected:\n%s\n\nGot:\n%s\n", expected.String(), output.String())
 	}
 }
